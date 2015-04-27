2949a7c51d8f230da7beb9099470866154a164b4
unknown <t.kikutis@gmail.com>
2015-04-27 21:40:25 +0300

    remove unneeded deps

--- src/server.js
+++ src/server.js
@@ -1,22 +1,36 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs');
-
-app.set('views', './');
-
 app.use('/', express.static('../static'));
 
 var React = require('react');
 var Router = require('react-router');
 var routes = require('../build/app/routes');
 
-// if using express it might look like this
 app.use(function (req, res) {
 	// pass in `req.url` and the router will immediately match
 	Router.run(routes, req.url, function (Handler) {
-		var content = React.renderToString(React.createElement(Handler, null));
-		res.render('index', {content: content});
+		var content = React.renderToString(<Handler/>);
+		var page = '\
+			<!DOCTYPE html>\
+			<html>\
+				<head>\
+					<meta charset="utf-8">\
+					<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">\
+				</head>\
+				<body>\
+					<div id="app">' + content + '</div>\
+					<script type="text/javascript" src="app.js"></script>\
+				</body>\
+			</html>\
+		';
+
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Content-Length': page.length
+		});
+		
+		res.end(page);
 	});
 });
 
